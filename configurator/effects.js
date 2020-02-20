ee2d810ea33f78c883c5274fff5c46defea732c6
t0m50n <tomsonforward1@gmail.com>
2020-02-20 21:24:04 +0000

    Started creating configurator for effects

---
+++ configurator/effects.js
@@ -0,0 +1,15 @@
+let effects = {
+	"text": "Root node 2",
+	"state": {
+		"opened": true,
+		"selected": true
+	},
+	"children": [
+		{
+			"text": "Child 1"
+		},
+		{
+			"text": "Child 1"
+		}
+	]
+}
