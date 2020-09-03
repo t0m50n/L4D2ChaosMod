fe2644196fb6f73bc858a85402dd938f43d7c167
t0m50n <tomsonforward1@gmail.com>
2020-09-03 14:56:05 +0100

    Build process now updates dependencies

--- build.py
+++ build.py
@@ -14,8 +14,16 @@ import subprocess
 import glob
 import shutil
 import os
-from collections import namedtuple
+import json
+import re
+from collections import namedtuple, defaultdict
+from urllib.parse import urljoin
+from datetime import datetime
 
+import requests
+from bs4 import BeautifulSoup
+
+import build_config
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Downloads third party plugins, compiles chaosmod \
@@ -32,18 +40,65 @@ def parse_args():
 
     return parser.parse_args()
 
+def download_dependencies(dependancies):
+    try:
+        with open('version_log.json', 'r') as f:
+            log = defaultdict(dict, json.load(f))
+    except OSError:
+        log = defaultdict(dict)
+    
+    for d in dependancies:
+        print(f'Downloading {d.name}')
+        html = requests.get(build_config.BASE_FORUM_URL + 'showpost.php?p=' + d.post)
+        soup = BeautifulSoup(html.text, 'html.parser')
+
+        url = next(a['href'] for a in soup.find_all('a') if d.link_predicate(a))
+        url = urljoin(build_config.BASE_FORUM_URL, url)
+        resp = requests.get(url)
+
+        d.install_func(resp=resp)
+        
+        version = get_dependancy_version(d, resp)
+        if 'version' in log[d.name] and log[d.name]['version'] != version:
+            print(f'Dependency {d.name} was updated from {log[d.name]["version"]} to {version}')
+        log[d.name]['date'] = datetime.utcnow()
+        log[d.name]['version'] = version
+    
+    with open('version_log.json', 'w') as f:
+        json.dump(log, f, sort_keys=True, indent=4, default=str)
+
+def get_dependancy_version(dependancy, resp):
+    if not dependancy.scripts:
+        # Use filename as version
+        return re.search(r'filename="?(.+)\.', resp.headers['content-disposition']).group(1)
+
+    # Get version from source code
+    script_path = os.path.join(build_config.BUILD_FOLDER, 'addons/sourcemod/scripting', dependancy.scripts[0])
+    with open(script_path, 'r') as f:
+        content = f.read()
+    
+    version = build_config.PLUGIN_VERSION_REGEX.search(content)
+    if version is None:
+        return '???'
+    else:
+        return version.group(1)
+
 def compile_scripts(comp_path, scripts):
+    # Must use backslashes here or spcomp doesn't find includes correctly
+    script_dir = os.path.join(build_config.BUILD_FOLDER, 'addons\\sourcemod\\scripting')
     for s in scripts:
-        # Must use backslashes here or spcomp doesn't find includes correctly
-        subprocess.run([comp_path, os.path.join('addons\\sourcemod\\scripting\\', s)], check=True)
+        subprocess.run([comp_path,
+                        os.path.join(script_dir, s),
+                        '-i',
+                        os.path.join(script_dir, 'include')], check=True)
     
     for plugin in glob.glob('*.smx'):
-        dest_path = os.path.join('addons/sourcemod/plugins', plugin)
+        dest_path = os.path.join(build_config.BUILD_FOLDER, 'addons/sourcemod/plugins', plugin)
         if os.path.isfile(dest_path):
             os.remove(dest_path)
         shutil.move(plugin, dest_path)
 
-def install(install_path):
+def copy_to_server(install_path):
     CopyOperation = namedtuple('CopyOperation', ['src', 'dst'])
     copy_ops = [
         CopyOperation(src='addons', dst='left4dead2/addons'),
@@ -52,7 +107,8 @@ def install(install_path):
     
     for c in copy_ops:
         try:
-            shutil.copytree(c.src, os.path.join(install_path, c.dst), dirs_exist_ok=True)
+            shutil.copytree(os.path.join(build_config.BUILD_FOLDER, c.src), os.path.join(install_path, c.dst),
+                            dirs_exist_ok=True, ignore=shutil.ignore_patterns('*.sp', '*.inc'))
         except shutil.Error as e:
             print('WARNING: The following copy errors were ignored while installing plugins:')
             for (src, dst, reason) in e.args[0]:
@@ -61,21 +117,24 @@ def install(install_path):
 if __name__ == '__main__':
     args = parse_args()
 
-    scripts = [
-        'chaos_mod.sp',
-        'chaos_mod_effects.sp'
-    ]
-    if args.THIRD_PARTY:
-        scripts += [
-            'l4d_damage.sp',
-            'l4d2_spawnuncommons.sp',
-            'security_entity_limit.sp',
-            'l4d2_autoIS.sp',
-            'left4dhooks.sp',
-            'WeaponHandling.sp'
-        ]
+    print('Copying chaosmod source files to build folder')
+    shutil.copytree('addons', os.path.join(build_config.BUILD_FOLDER, 'addons'), dirs_exist_ok=True)
+    shutil.copytree('cfg', os.path.join(build_config.BUILD_FOLDER, 'cfg'), dirs_exist_ok=True)
 
+    if args.THIRD_PARTY:
+        print('Downloading dependencies to build folder')
+        download_dependencies(build_config.dependencies)
+
+    # Generate list of scripts to be compiled
+    scripts = build_config.first_scripts
+    if args.THIRD_PARTY:
+        for d in build_config.dependencies:
+            scripts += d.scripts
+
+    print('Compiling scripts')
     compile_scripts(args.SP_COMP, scripts)
 
+    # Copy files to your server installation
     if args.INSTALL_PATH is not None:
-        install(args.INSTALL_PATH)
+        print('Copying files to server install')
+        copy_to_server(args.INSTALL_PATH)
---
+++ build_config.py
@@ -0,0 +1,77 @@
+import functools
+import os
+import io
+import re
+from collections import namedtuple
+from zipfile import ZipFile
+
+BASE_FORUM_URL = 'https://forums.alliedmods.net/'
+BUILD_FOLDER = 'build'
+PLUGIN_VERSION_REGEX = re.compile(r'#define[^"\nA-Za-z_]*?PLUGIN_VERSION[^"\n]*?"([^"\n]*?)"')
+
+first_scripts = [
+    'chaos_mod.sp',
+    'chaos_mod_effects.sp',
+    'security_entity_limit.sp'
+]
+
+def install(path, is_zip, resp):
+    if is_zip:
+        with ZipFile(io.BytesIO(resp.content)) as z:
+            z.extractall(os.path.join(BUILD_FOLDER, path))
+    else:
+        with open(os.path.join(BUILD_FOLDER, path), 'wb') as f:
+            f.write(resp.content)
+
+Dependency = namedtuple('Dependency', ['name', 'post', 'link_predicate', 'install_func', 'scripts'])
+dependencies = [
+    Dependency(
+        name='dhooks',
+        post='2588686',
+        link_predicate=lambda a: 'sm110.zip' in a.text,
+        install_func=functools.partial(install, path='', is_zip=True),
+        scripts=[]),
+
+    Dependency(
+        name='l4dhooks',
+        post='2684862',
+        link_predicate=lambda a: '.zip' in a.text,
+        install_func=functools.partial(install, path='addons', is_zip=True),
+        scripts=['left4dhooks.sp']),
+
+    Dependency(
+        name='weapon_damage_mod',
+        post='1066911',
+        link_predicate=lambda a: a.text == 'Get Source',
+        install_func=functools.partial(install,
+                                       path='addons/sourcemod/scripting/l4d_damage.sp',
+                                       is_zip=False),
+        scripts=['l4d_damage.sp']),
+
+    Dependency(
+        name='spawn_uncommons',
+        post='993523',
+        link_predicate=lambda a: a.text == 'Get Source',
+        install_func=functools.partial(install,
+                                       path='addons/sourcemod/scripting/l4d2_spawnuncommons.sp',
+                                       is_zip=False),
+        scripts=['l4d2_spawnuncommons.sp']),
+
+    Dependency(
+        name='auto_infected_spawner',
+        post='954529',
+        link_predicate=lambda a: a.text == 'Get Source' and 'l4d2' in a.parent.text,
+        install_func=functools.partial(install,
+                                       path='addons/sourcemod/scripting/l4d2_autoIS.sp',
+                                       is_zip=False),
+        scripts=['l4d2_autoIS.sp']),
+
+    Dependency(
+        name='weapon_handling_api',
+        post='2674761',
+        link_predicate=lambda a: 'weapon' in a.text.lower() and '.zip' in a.text,
+        install_func=functools.partial(install,
+                                       path='',
+                                       is_zip=True),
+        scripts=['WeaponHandling.sp']),
+]
